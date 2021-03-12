ef25430050416c47b0a427da512bd624050cf101
Arnaud Millan <a.millan@fonroche-lighting.com>
2021-03-12 09:21:47 +0000

    Initial commit

---
+++ dev/dev_history.R
@@ -0,0 +1,43 @@
+# On sauve le processus de développement
+# fonctions à executer pendant le dev
+
+# cacher ce fichier au build
+usethis::use_build_ignore("dev/dev_history.R")
+
+# définir la licence
+usethis::use_gpl3_license("arnaudmillan")
+
+# créer une vignette pour documenter l'utilisation
+usethis::use_vignette("mes-fonctions")
+
+# initialiser git
+usethis::use_git()
+
+# créer le fichier R d'une foncrion dans /R
+usethis::use_r("dire_bonjour")
+usethis::use_r("get_6_first_lines")
+# Créer un fichier d'explication rapide du package
+usethis::use_readme_rmd()
+usethis::use_gitlab_ci()
+# Créer un fichier pour les tests des fonctions du package
+usethis::use_testthat()
+# Créer un test
+usethis::use_test(name = "get_6_first_lines")
+
+# encapsuler des datas
+usethis::use_data_raw("my_data")
+usethis::use_r("doc_my_data")
+# utilisation données brutes dans int
+dir.create("inst")
+dir.create("inst/extdata")
+
+usethis::use_news_md()
+
+# Je teste ma vignette
+devtools::build_vignettes()
+# Outil pour gérer les dépendances
+attachment::att_amend_desc()
+
+devtools::test()
+# Vérifier le package
+devtools::check()
